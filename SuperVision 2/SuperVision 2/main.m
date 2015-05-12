b8d17f0ea9e0b9f71ab4cd177aeab015b1e7dd8e
Pengfei Tan <tanpf5@gmail.com>
2015-05-12 11:46:41 -0400

    Revert ad828d0..48af6b7
    This rolls back to commit ad828d0b5fea295af395b5eac7ab0ca824bcfbe7.

---
+++ SuperVision 2/SuperVision 2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SuperVision 2
+//
+//  Created by 谭鹏飞 on 5/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
