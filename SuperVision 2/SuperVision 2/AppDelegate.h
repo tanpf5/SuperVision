ad828d0b5fea295af395b5eac7ab0ca824bcfbe7
Pengfei Tan <tanpf5@gmail.com>
2015-05-11 21:25:34 -0400

    1.0

---
+++ SuperVision 2/SuperVision 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperVision 2
+//
+//  Created by 谭鹏飞 on 5/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuperVision 2/SuperVision 2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperVision 2
+//
+//  Created by 谭鹏飞 on 5/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
