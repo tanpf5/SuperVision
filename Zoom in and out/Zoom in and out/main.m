72782689afdd5c5e97a8a841b2960a0e86ab4f2b
Pengfei Tan <tanpf5@gmail.com>
2015-05-26 15:35:12 -0400

    add zoom in/out

---
+++ Zoom in and out/Zoom in and out/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Zoom in and out
+//
+//  Created by Pengfei Tan on 5/26/15.
+//  Copyright (c) 2015 Massachusetts Eye and Ear Infirmary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
