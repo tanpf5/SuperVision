d815bb8b8b423ec16db88259ac79eb5911c69860
Pengfei Tan <tanpf5@gmail.com>
2015-06-26 17:44:46 -0400

    6.26

---
+++ SuperVision+ Goggles/SuperVision+ Goggles/Model/SVScrollView.h
@@ -0,0 +1,13 @@
+//
+//  SVScrollView.h
+//  SuperVision+ Goggles
+//
+//  Created by Pengfei Tan on 6/26/15.
+//  Copyright (c) 2015 Massachusetts Eye and Ear Infirmary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVScrollView : UIScrollView
+
+@end
---
+++ SuperVision+ Goggles/SuperVision+ Goggles/Model/SVScrollView.m
@@ -0,0 +1,21 @@
+//
+//  SVScrollView.m
+//  SuperVision+ Goggles
+//
+//  Created by Pengfei Tan on 6/26/15.
+//  Copyright (c) 2015 Massachusetts Eye and Ear Infirmary. All rights reserved.
+//
+
+#import "SVScrollView.h"
+
+@implementation SVScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
